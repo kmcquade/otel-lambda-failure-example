7ba38445d68e33ea7088855371479ffaf4d36e05
kmcquade <kinnairdm@gmail.com>
2024-06-19 16:49:44 -0400

    Add boto3 to the Lambda so a successful case can be demonstrated.

--- src/hello_world/example.py
+++ src/hello_world/example.py
@@ -1,4 +1,5 @@
 import json
+import boto3
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
@@ -13,6 +14,16 @@ tracer = trace.get_tracer(__name__)
 def lambda_handler(event, context):
     with tracer.start_as_current_span("example_span"):
         print("Hello World")
+        # This will issue an STS call, which should be filtered out
+        sts_client = boto3.client("sts")
+        account_id = sts_client.get_caller_identity()["Account"]
+        print("Account ID:", account_id)
+
+        # This will list all S3 buckets in the account, which should be filtered out
+        s3_client = boto3.client("s3")
+        response = s3_client.list_buckets()
+        print("S3 Buckets:", response["Buckets"])
+
         return {
             'statusCode': 200,
             'body': json.dumps('Hello from Lambda!')
