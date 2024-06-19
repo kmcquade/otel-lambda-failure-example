219a01e5e9784318f0edc7cd27d6e427cd0aa560
kmcquade <kinnairdm@gmail.com>
2024-06-19 16:41:25 -0400

    Add OpenTelemetry code to the Lambda so a successful case can be demonstrated.

--- src/hello_world/example.py
+++ src/hello_world/example.py
@@ -1,8 +1,19 @@
 import json
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
+tracer = trace.get_tracer(__name__)
 
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    with tracer.start_as_current_span("example_span"):
+        print("Hello World")
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
