217886df37b1e4d5cdc479a78a5b5eb99aa08deb
kmcquade <kinnairdm@gmail.com>
2024-06-19 16:50:50 -0400

    Add reference to the honeycomb key

--- src/hello_world/example.py
+++ src/hello_world/example.py
@@ -3,6 +3,7 @@ import boto3
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+from opentelemetry.trace import SpanKind, Link, format_span_id, format_trace_id, NonRecordingSpan, SpanContext
 
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
@@ -12,12 +13,18 @@ tracer = trace.get_tracer(__name__)
 
 
 def lambda_handler(event, context):
-    with tracer.start_as_current_span("example_span"):
+    with tracer.start_as_current_span("parent_span") as span:
+        span_context: SpanContext = span.get_span_context()
+        otel_context = trace.set_span_in_context(NonRecordingSpan(span_context))
+
         print("Hello World")
+        with tracer.start_as_current_span("child_span", context=otel_context):
+            print("Hello World from Child Span")
+
         # This will issue an STS call, which should be filtered out
         sts_client = boto3.client("sts")
         account_id = sts_client.get_caller_identity()["Account"]
-        print("Account ID:", account_id)
+        print(f"Account ID: {account_id}")
 
         # This will list all S3 buckets in the account, which should be filtered out
         s3_client = boto3.client("s3")
